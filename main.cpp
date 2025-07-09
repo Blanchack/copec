cb91ad4f43ec245b541a5fb52b26f292e1d9644c
vicente132 <vihernandez2022@udec.cl>
2025-07-09 07:32:43 -0400

    olaa

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string command;
+
+    // prompt para el comando
+    // para este tipo de prompts, no se necesitan llamadas a sistema
+    cout << "Enter a command: ";
+
+    // para weas mas dinamicas se deben usar las llamadas a sistema
+
+    cin >> command;
+}
